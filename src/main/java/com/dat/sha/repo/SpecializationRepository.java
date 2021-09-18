1336cb6ddb002e28674ce7dc5433abd290afa68b
sathishstdev123 <51478783+sathishstdev123@users.noreply.github.com>
2021-09-18 12:23:53 +0530

    spec repo update

---
+++ src/main/java/com/dat/sha/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.dat.sha.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dat.shaEntity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long>  {
+
+}
